4f9449e4f8293614ff638e99eb2303a6d5ef82d6
imagine <15011240224@163.com>
2018-01-29 22:37:43 +0800

    init

---
+++ smartFreamwork/src/main/java/org/smart4j/chapter2/model/Customer.java
@@ -0,0 +1,58 @@
+package org.smart4j.chapter2.model;
+
+public class Customer {
+    private long id;
+    private String name;
+    private String contact;
+    private String telephone;
+    private String email;
+    private String remark;
+
+    public long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getContact() {
+        return contact;
+    }
+
+    public String getTelephone() {
+        return telephone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getRemark() {
+        return remark;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setContact(String contact) {
+        this.contact = contact;
+    }
+
+    public void setTelephone(String telephone) {
+        this.telephone = telephone;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setRemark(String remark) {
+        this.remark = remark;
+    }
+}
---
+++ smartFreamwork/src/main/webapp/WEB-INF/jsp/customer_create.jsp
@@ -0,0 +1,9 @@
+<%@ page pageEncoding="UTF-8" %>
+<html>
+<head>
+<title>创建客户</title>
+</head>
+<body>
+<h1>Hello!zhouwenjun</h1>
+</body>
+</html>
